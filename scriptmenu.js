e5227cb2d2ea446e57356d3365c7dd1e0c9552d6
Roberto Eduardo <robertoemd@gmail.com>
2026-05-28 13:00:05 -0300

    :hammer: teste de ajsute

--- scriptmenu.js
+++ scriptmenu.js
@@ -20,26 +20,3 @@ function copiarPix() {
     iconwwp.style.opacity = "0";
    
   }
-window.addEventListener('load', async () => {
-    // 1. Log para saber se a página carregou e o script rodou
-    console.log("Página carregada. Verificando hash...");
-
-    const hash = window.location.hash;
-    console.log("Hash atual:", hash); // Isso deve mostrar algo com 'access_token'
-
-    if (hash.includes('type=recovery')) {
-        console.log("Token de recuperação detectado!");
-        
-        const novaSenha = prompt("Link validado! Digite sua nova senha:");
-        
-        if (novaSenha) {
-            const { error } = await supabaseClient.auth.updateUser({ password: novaSenha });
-            if (error) {
-                alert("Erro ao atualizar senha: " + error.message);
-            } else {
-                alert("Senha alterada com sucesso!");
-                window.location.hash = ""; // Limpa a URL para não pedir de novo
-            }
-        }
-    }
-});
